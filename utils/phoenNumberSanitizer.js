5b3e30e5666cd042d07b1734eb760ec47060ed18
ashish17133 <ashishdhakal6@gmail.com>
2022-03-24 12:54:10 +0545

    Change in regx for phoneNumber sanitize

--- utils/phoenNumberSanitizer.js
+++ utils/phoenNumberSanitizer.js
@@ -1,6 +1,15 @@
 const sanitizePhoneNumber=function sanitizePhoneNumber(number) {
     let _tempNumber=number;
-    _tempNumber= _tempNumber.replaceAll("-", "").replaceAll("!", "").replaceAll("'", "").replaceAll(",", "").replaceAll("_", "").replaceAll("+", "").replaceAll("(", "").replaceAll(")", "").replaceAll(" ", "");
+    const regx1=/-/g;
+    const regx2=/!/g;
+    const regx3=/'/g;
+    const regx4=/,/g;
+    const regx5=/_/g;
+    const regx6=/+/g;
+    const regx7=/\(/g;
+    const regx8=/)/g; 
+    const regx9=/ /g;   
+    _tempNumber= _tempNumber.replace(regx1, "").replace(regx2, "").replace(regx3, "").replace(regx4, "").replace(regx5, "").replace(regx6, "").replace(regx7, "").replace(regx8, "").replace(regx9, "");
     _length=_tempNumber.length;
     _start=_length-10;
     _tempNumber=_tempNumber.substring(_start,_length);
