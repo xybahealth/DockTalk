8e562fea5f2eec5af91fca1e99d8aaae81d0a00e
ashish17133 <ashishdhakal6@gmail.com>
2022-03-24 12:43:04 +0545

    Error resolving in sanitize phone nuMber

--- utils/phoenNumberSanitizer.js
+++ utils/phoenNumberSanitizer.js
@@ -1,14 +1,6 @@
 const sanitizePhoneNumber=function sanitizePhoneNumber(number) {
     let _tempNumber=number;
-    _tempNumber= _tempNumber.replaceAll("-", "")
-    .replaceAll("!", "")
-    .replaceAll("'", "")
-    .replaceAll(",", "")
-    .replaceAll("_", "")
-    .replaceAll("+", "")
-    .replaceAll("(", "")
-    .replaceAll(")", "")
-    .replaceAll(" ", "");
+    _tempNumber= _tempNumber.replaceAll("-", "").replaceAll("!", "").replaceAll("'", "").replaceAll(",", "").replaceAll("_", "").replaceAll("+", "").replaceAll("(", "").replaceAll(")", "").replaceAll(" ", "");
     _length=_tempNumber.length;
     _start=_length-10;
     _tempNumber=_tempNumber.substring(_start,_length);
